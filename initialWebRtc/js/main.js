4da5ab8e0551905cb53b4a433db0411afb70e01d
nbappi <bappi.cse562@gmail.com>
2016-09-19 18:47:35 +0600

    Basic getUserMedia

--- initialWebRtc/js/main.js
+++ initialWebRtc/js/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+var options = {
+   audio : false,
+   video : true
+};
+var videoDoc = document.getElementById('captureVideo');
+
+function streaming(stream){
+	videoDoc.src =  window.URL.createObjectURL(stream);	
+}
+
+function error(err){
+  console.log(err);
+}
+
+navigator.mediaDevices.getUserMedia(options)
+.then(streaming)
+.catch(error);
